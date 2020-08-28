d2eecdf32700eb8a2692f32ffa928c661714177e
sdonohue0918 <sdonohue0918@gmail.com>
2020-08-28 05:57:59 -0400

    all rspec passing for Has Many Objects Through Lab

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,40 @@
 class Customer
-  
+    attr_accessor :name, :age
+   
+    @@all = []
+   
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @@all << self
+    end
+   
+    def self.all
+      @@all
+    end
+
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+      end
+
+    #   If you take a look at our customer right now, they aren't capable of doing much. Let's change that and give them the ability to create a meal. To do this, they'll need to take in an instance of a waiter and supply the total and tip, which we'll have defaulted to 0 here as well:
+
+    def meals
+        Meal.all.select do |meal|
+          meal.customer == self
+        end
+      end
+
+      def waiters
+        meals.map do |meal|
+          meal.waiter
+        end
+      end
+
+      #So rachel.meals will return an array of all of Rachel's meals, but what if we now want a list of all of the waiters that Rachel has interacted with? Each meal is also associated with a waiter, so to get every waiter from every meal Rachel has had, we need to take the array of all of Rachel's meals, map over it, getting the waiter from each of those meals.
+
+    #   Since we already have a #meals method to get an array of meals, we can reuse it here and write a #waiters method like the following:
+
+
+   
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,50 @@
 class Waiter
+ 
+    attr_accessor :name, :yrs_experience
+   
+    @@all = []
+   
+    def initialize(name, yrs_experience)
+      @name = name
+      @yrs_experience = yrs_experience
+      @@all << self
+    end
+   
+    def self.all
+      @@all
+    end
 
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+      end
+
+    #   As you can see, we don't need to take customer in as an argument, because we're passing in self as a reference to the current instance of customer. This method will allow us to create new meals as a customer, and automatically associate each new meal with the customer that created it. We can do the same thing for the Waiter class:
+
+
+    def meals
+        Meal.all.select do |meal|
+          meal.waiter == self #checking for waiter now
+        end
+      end
+
+
+      #To reinforce this concept, let's look at the same sort of relationship, but in the other direction. This time, we will build out methods so a waiter can find the customer that tips the the best.
+
+    #Again to start, just like the customer, the waiter needs a way to get all the meals they have served. We'll create a #meals method again, with a subtle change:
+
+
+    def best_tipper
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+          meal_a.tip <=> meal_b.tip
+        end
+       
+        best_tipped_meal.customer
+      end
+
+
+      #To find the best tipper, we can write another method, #best_tipper, use the array we get from #meals, then return the customer of the meal with the highest tip:
+
+
+   
 end
